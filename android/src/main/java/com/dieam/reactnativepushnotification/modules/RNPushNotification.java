fd2196025e6189348a40660e90de21023e27d14d
Philip Heinser <philipheinser@me.com>
2016-10-13 01:51:19 +0200

    Initial

---
+++ android/src/main/java/com/dieam/reactnativepushnotification/modules/RNPushNotification.java
@@ -0,0 +1,171 @@
+package com.dieam.reactnativepushnotification.modules;
+
+import android.app.Activity;
+import android.app.Application;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Build;
+import android.os.Bundle;
+
+import com.facebook.react.bridge.ActivityEventListener;
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class RNPushNotification extends ReactContextBaseJavaModule implements ActivityEventListener {
+    private RNPushNotificationHelper mRNPushNotificationHelper;
+
+    public RNPushNotification(ReactApplicationContext reactContext) {
+        super(reactContext);
+
+        reactContext.addActivityEventListener(this);
+        mRNPushNotificationHelper = new RNPushNotificationHelper((Application) reactContext.getApplicationContext());
+        registerNotificationsRegistration();
+        registerNotificationsReceiveNotification();
+    }
+
+    @Override
+    public String getName() {
+        return "RNPushNotification";
+    }
+
+    @Override
+    public Map<String, Object> getConstants() {
+        final Map<String, Object> constants = new HashMap<>();
+
+        return constants;
+    }
+
+    private void sendEvent(String eventName, Object params) {
+        ReactContext reactContext = getReactApplicationContext();
+
+        if (reactContext.hasActiveCatalystInstance()) {
+            reactContext
+                    .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                    .emit(eventName, params);
+        }
+    }
+
+    public void onNewIntent(Intent intent) {
+        if (intent.hasExtra("notification")) {
+            Bundle bundle = intent.getBundleExtra("notification");
+            bundle.putBoolean("foreground", false);
+            intent.putExtra("notification", bundle);
+            notifyNotification(bundle);
+        }
+    }
+
+    private void registerNotificationsRegistration() {
+        IntentFilter intentFilter = new IntentFilter(getReactApplicationContext().getPackageName() + ".RNPushNotificationRegisteredToken");
+
+        getReactApplicationContext().registerReceiver(new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                String token = intent.getStringExtra("token");
+                WritableMap params = Arguments.createMap();
+                params.putString("deviceToken", token);
+
+                sendEvent("remoteNotificationsRegistered", params);
+            }
+        }, intentFilter);
+    }
+
+    private void registerNotificationsReceiveNotification() {
+        IntentFilter intentFilter = new IntentFilter(getReactApplicationContext().getPackageName() + ".RNPushNotificationReceiveNotification");
+        getReactApplicationContext().registerReceiver(new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+               notifyNotification(intent.getBundleExtra("notification"));
+            }
+        }, intentFilter);
+    }
+
+    private void notifyNotification(Bundle bundle) {
+        String bundleString = convertJSON(bundle);
+
+        WritableMap params = Arguments.createMap();
+        params.putString("dataJSON", bundleString);
+
+        sendEvent("remoteNotificationReceived", params);
+    }
+
+    private String convertJSON(Bundle bundle) {
+        JSONObject json = new JSONObject();
+        Set<String> keys = bundle.keySet();
+        for (String key : keys) {
+            try {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                    json.put(key, JSONObject.wrap(bundle.get(key)));
+                } else {
+                    json.put(key, bundle.get(key));
+                }
+            } catch (JSONException e) {
+                return null;
+            }
+        }
+        return json.toString();
+    }
+
+    @ReactMethod
+    public void requestPermissions(String senderID) {
+        ReactContext reactContext = getReactApplicationContext();
+
+        Intent GCMService = new Intent(reactContext, RNPushNotificationRegistrationService.class);
+
+        GCMService.putExtra("senderID", senderID);
+        reactContext.startService(GCMService);
+    }
+
+    @ReactMethod
+    public void cancelAllLocalNotifications() {
+        mRNPushNotificationHelper.cancelAll();
+    }
+
+    @ReactMethod
+    public void presentLocalNotification(ReadableMap details) {
+        Bundle bundle = Arguments.toBundle(details);
+        mRNPushNotificationHelper.sendNotification(bundle);
+    }
+
+    @ReactMethod
+    public void scheduleLocalNotification(ReadableMap details) {
+        Bundle bundle = Arguments.toBundle(details);
+        mRNPushNotificationHelper.sendNotificationScheduled(bundle);
+    }
+
+    @ReactMethod
+    public void getInitialNotification(Promise promise) {
+        WritableMap params = Arguments.createMap();
+        Activity activity = getCurrentActivity();
+        if (activity != null) {
+            Intent intent = activity.getIntent();
+            Bundle bundle = intent.getBundleExtra("notification");
+            if (bundle != null) {
+                bundle.putBoolean("foreground", false);
+                String bundleString = convertJSON(bundle);
+                params.putString("dataJSON", bundleString);
+            }
+        }
+        promise.resolve(params);
+    }
+
+    @Override
+    public void onActivityResult(Activity activity, int requestCode, int resultCode, Intent data) {
+        // Ignored, required to implement ActivityEventListener
+    }
+}
---
+++ android/src/main/java/com/dieam/reactnativepushnotification/modules/RNPushNotificationHelper.java
@@ -0,0 +1,319 @@
+package com.dieam.reactnativepushnotification.modules;
+
+import android.support.annotation.Nullable;
+import android.app.*;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import com.facebook.common.executors.CallerThreadExecutor;
+import com.facebook.common.references.CloseableReference;
+import com.facebook.datasource.DataSource;
+import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.imagepipeline.common.Priority;
+import com.facebook.imagepipeline.core.ImagePipeline;
+import com.facebook.imagepipeline.datasource.BaseBitmapDataSubscriber;
+import com.facebook.imagepipeline.image.CloseableImage;
+import com.facebook.imagepipeline.request.ImageRequest;
+import com.facebook.imagepipeline.request.ImageRequestBuilder;
+
+public class RNPushNotificationHelper {
+    private static final long DEFAULT_VIBRATION = 300L;
+    private static final String TAG = RNPushNotificationHelper.class.getSimpleName();
+
+    private Context mContext;
+
+    public RNPushNotificationHelper(Application context) {
+        mContext = context;
+    }
+
+    public Class getMainActivityClass() {
+        String packageName = mContext.getPackageName();
+        Intent launchIntent = mContext.getPackageManager().getLaunchIntentForPackage(packageName);
+        String className = launchIntent.getComponent().getClassName();
+        try {
+            return Class.forName(className);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private AlarmManager getAlarmManager() {
+        return (AlarmManager) mContext.getSystemService(Context.ALARM_SERVICE);
+    }
+
+    private PendingIntent getScheduleNotificationIntent(Bundle bundle) {
+        int notificationID;
+        String notificationIDString = bundle.getString("id");
+
+        if (notificationIDString != null) {
+            notificationID = Integer.parseInt(notificationIDString);
+        } else {
+            notificationID = (int) System.currentTimeMillis();
+        }
+
+        Intent notificationIntent = new Intent(mContext, RNPushNotificationPublisher.class);
+        notificationIntent.putExtra(RNPushNotificationPublisher.NOTIFICATION_ID, notificationID);
+        notificationIntent.putExtras(bundle);
+
+        return PendingIntent.getBroadcast(mContext, notificationID, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+    }
+
+    public void sendNotificationScheduled(Bundle bundle) {
+        Class intentClass = getMainActivityClass();
+        if (intentClass == null) {
+            return;
+        }
+
+        Double fireDateDouble = bundle.getDouble("fireDate", 0);
+        if (fireDateDouble == 0) {
+            return;
+        }
+
+        long fireDate = Math.round(fireDateDouble);
+        long currentTime = System.currentTimeMillis();
+
+        Log.i("ReactSystemNotification", "fireDate: " + fireDate + ", Now Time: " + currentTime);
+        PendingIntent pendingIntent = getScheduleNotificationIntent(bundle);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            getAlarmManager().setExact(AlarmManager.RTC_WAKEUP, fireDate, pendingIntent);
+        } else {
+            getAlarmManager().set(AlarmManager.RTC_WAKEUP, fireDate, pendingIntent);
+        }
+    }
+
+    public void sendNotification(final Bundle bundle) {
+        String imageUrl = bundle.getString("image_url");
+        if( imageUrl == null ){
+            sendNotificationWithImage( bundle, null );
+            return;
+        }
+        ImagePipeline imagePipeline = Fresco.getImagePipeline();
+
+        ImageRequest imageRequest = ImageRequestBuilder
+                .newBuilderWithSource( Uri.parse(imageUrl) )
+                .setRequestPriority(Priority.HIGH)
+                .setLowestPermittedRequestLevel(ImageRequest.RequestLevel.FULL_FETCH)
+                .build();
+        DataSource<CloseableReference<CloseableImage>> dataSource =
+                imagePipeline.fetchDecodedImage(imageRequest, mContext);
+
+
+        dataSource.subscribe(new BaseBitmapDataSubscriber() {
+            @Override
+            public void onNewResultImpl(@Nullable Bitmap bitmap) {
+                if (bitmap == null) {
+                    Log.d(TAG, "Bitmap data source returned success, but bitmap null.");
+                    sendNotificationWithImage(bundle, null);
+                    return;
+                }
+                sendNotificationWithImage( bundle, bitmap );
+            }
+
+            @Override
+            public void onFailureImpl(DataSource dataSource) {
+                sendNotificationWithImage( bundle, null );
+            }
+        }, CallerThreadExecutor.getInstance());
+
+
+    }
+
+    public void sendNotificationWithImage(Bundle bundle, Bitmap image ) {
+        try {
+            Class intentClass = getMainActivityClass();
+            if (intentClass == null) {
+                return;
+            }
+
+            if (bundle.getString("message") == null) {
+                return;
+            }
+
+            Resources res = mContext.getResources();
+            String packageName = mContext.getPackageName();
+
+            String message = bundle.getString("message");
+            String title = bundle.getString("title");
+            if (title == null) {
+                ApplicationInfo appInfo = mContext.getApplicationInfo();
+                title = mContext.getPackageManager().getApplicationLabel(appInfo).toString();
+            }
+
+            NotificationCompat.Builder notification = new NotificationCompat.Builder(mContext)
+                    .setContentTitle(title)
+                    .setTicker(bundle.getString("ticker"))
+                    .setVisibility(NotificationCompat.VISIBILITY_PRIVATE)
+                    .setPriority(NotificationCompat.PRIORITY_HIGH)
+                    .setAutoCancel(bundle.getBoolean("autoCancel", true));
+
+            String group = bundle.getString("group");
+            if (group != null) {
+                notification.setGroup(group);
+            }
+
+            String largeIcon = bundle.getString("largeIcon");
+
+            String subText = bundle.getString("subText");
+
+            if (subText != null) {
+                notification.setSubText(subText);
+            }
+
+            if (bundle.containsKey("number")) {
+                try {
+                    int number = (int) bundle.getDouble("number");
+                    notification.setNumber(number);
+                } catch (Exception e) {
+                    String numberAsString = bundle.getString("number");
+                    if(numberAsString != null) {
+                        int number = Integer.parseInt(numberAsString);
+                        notification.setNumber(number);
+                        Log.w(TAG, "'number' field set as a string instead of an int");
+                    }
+                }
+            }
+
+            int smallIconResId;
+            int largeIconResId;
+
+            String smallIcon = bundle.getString("smallIcon");
+
+            if (smallIcon != null) {
+                smallIconResId = res.getIdentifier(smallIcon, "mipmap", packageName);
+            } else {
+                smallIconResId = res.getIdentifier("ic_notification", "mipmap", packageName);
+            }
+
+            if (smallIconResId == 0) {
+                smallIconResId = res.getIdentifier("ic_launcher", "mipmap", packageName);
+
+                if (smallIconResId == 0) {
+                    smallIconResId = android.R.drawable.ic_dialog_info;
+                }
+            }
+
+            if (largeIcon != null) {
+                largeIconResId = res.getIdentifier(largeIcon, "mipmap", packageName);
+            } else {
+                largeIconResId = res.getIdentifier("ic_launcher", "mipmap", packageName);
+            }
+
+            Bitmap largeIconBitmap = BitmapFactory.decodeResource(res, largeIconResId);
+
+            if (largeIconResId != 0 && (largeIcon != null || android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP)) {
+                notification.setLargeIcon(largeIconBitmap);
+            }
+
+            notification.setSmallIcon(smallIconResId);
+            String bigText = bundle.getString("bigText");
+
+            if (bigText == null) {
+                bigText = bundle.getString("message");
+            }
+
+            if( image != null ){
+              notification.setStyle(
+                    new NotificationCompat.BigPictureStyle()
+                            .bigPicture(image)
+                            .setBigContentTitle( title )
+                            .setSummaryText( message )
+                            );
+            } else {
+                notification.setContentText(message);
+                notification.setStyle(new NotificationCompat.BigTextStyle().bigText(bigText));
+            }
+
+            Intent intent = new Intent(mContext, intentClass);
+            intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+            bundle.putBoolean("userInteraction", true);
+            intent.putExtra("notification", bundle);
+
+            if (!bundle.containsKey("playSound") || bundle.getBoolean("playSound")) {
+                Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+                notification.setSound(defaultSoundUri);
+            }
+
+            if (bundle.containsKey("ongoing") || bundle.getBoolean("ongoing")) {
+                notification.setOngoing(bundle.getBoolean("ongoing"));
+            }
+
+            if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                notification.setCategory(NotificationCompat.CATEGORY_CALL);
+
+                String color = bundle.getString("color");
+                if (color != null) {
+                    notification.setColor(Color.parseColor(color));
+                }
+            }
+
+            int notificationID = (int) System.currentTimeMillis();
+            if (bundle.containsKey("id")) {
+                try {
+                    notificationID = (int) bundle.getDouble("id");
+                } catch (Exception e) {
+                    String notificationIDString = bundle.getString("id");
+
+                    if (notificationIDString != null) {
+                        Log.w(TAG, "'id' field set as a string instead of an int");
+
+                        try {
+                            notificationID = Integer.parseInt(notificationIDString);
+                        } catch (NumberFormatException nfe) {
+                            Log.w(TAG, "'id' field could not be converted to an int, ignoring it", nfe);
+                        }
+                    }
+                }
+            }
+
+            PendingIntent pendingIntent = PendingIntent.getActivity(mContext, notificationID, intent,
+                    PendingIntent.FLAG_UPDATE_CURRENT);
+
+            NotificationManager notificationManager =
+                    (NotificationManager) mContext.getSystemService(Context.NOTIFICATION_SERVICE);
+
+            notification.setContentIntent(pendingIntent);
+
+            if (!bundle.containsKey("vibrate") || bundle.getBoolean("vibrate")) {
+                long vibration = bundle.containsKey("vibration") ? (long) bundle.getDouble("vibration") : DEFAULT_VIBRATION;
+                if (vibration == 0)
+                    vibration = DEFAULT_VIBRATION;
+                notification.setVibrate(new long[]{0, vibration});
+            }
+
+            Notification info = notification.build();
+            info.defaults |= Notification.DEFAULT_LIGHTS;
+
+            if (bundle.containsKey("tag")) {
+                String tag = bundle.getString("tag");
+                notificationManager.notify(tag, notificationID, info);
+            } else {
+                notificationManager.notify(notificationID, info);
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "failed to send push notification", e);
+        }
+    }
+
+    public void cancelAll() {
+        NotificationManager notificationManager =
+                (NotificationManager) mContext.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.cancelAll();
+
+        Bundle b = new Bundle();
+        b.putString("id", "0");
+        getAlarmManager().cancel(getScheduleNotificationIntent(b));
+    }
+}
